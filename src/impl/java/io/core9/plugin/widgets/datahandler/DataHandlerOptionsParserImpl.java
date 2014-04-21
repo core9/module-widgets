6f2e1490e26ba0babd5d6a0ee3d1588cebdf51c0
Mark Wienk <markwienk@gmail.com>
2014-04-21 21:14:06 +0200

    Removed syso statements

--- src/impl/java/io/core9/plugin/widgets/datahandler/DataHandlerOptionsParserImpl.java
+++ src/impl/java/io/core9/plugin/widgets/datahandler/DataHandlerOptionsParserImpl.java
@@ -44,7 +44,6 @@ public class DataHandlerOptionsParserImpl implements DataHandlerOptionsParser {
 					fieldJson.put("label", field.getName());
 					result.put(field.getName(), fieldJson);
 				}
-				System.out.println(field.getType());
 			}
 		}
 	}

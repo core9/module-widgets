014569249c0454337fd330e20a186372332e1726
mwienk <mark.wienk@core9.io>
2014-06-19 17:43:43 +0200

    added subscribe to bundle

--- src/impl/java/io/core9/plugin/widgets/datahandler/factories/BundleDataHandlerFactoryImpl.java
+++ src/impl/java/io/core9/plugin/widgets/datahandler/factories/BundleDataHandlerFactoryImpl.java
@@ -7,16 +7,13 @@ import io.core9.plugin.widgets.datahandler.DataHandlerFactoryConfig;
 import io.core9.plugin.widgets.widget.Widget;
 import io.core9.plugin.widgets.widget.WidgetFactory;
 
-import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 import net.xeoh.plugins.base.annotations.injections.InjectPlugin;
 import rx.Observable;
 import rx.functions.Action1;
-import rx.functions.Func1;
 
 @PluginImplementation
 public class BundleDataHandlerFactoryImpl implements BundleDataHandler<BundleDataHandlerFactoryOptions> {
@@ -45,40 +42,29 @@ public class BundleDataHandlerFactoryImpl implements BundleDataHandler<BundleDat
 				if(options.getComponents() == null) {
 					return result;
 				}
-				Observable.from(options.getComponents()).parallel(new Func1<Observable<Component>, Observable<Map.Entry<String,Object>>>() {
+				
+				//TODO Use Parallel, why does it crash?
+				Observable.from(options.getComponents()).subscribe(new Action1<Component>() {
 
 					@Override
-					public Observable<Entry<String, Object>> call(Observable<Component> obs) {
-						return obs.map(new Func1<Component, Map.Entry<String,Object>>() {
-
-							@Override
-							public Entry<String, Object> call(Component component) {
-								if(component.getGlobals().size() > 0) {
-									for(Map.Entry<String,String> entry : component.getGlobals().entrySet()) {
-										if(entry.getValue().startsWith(":")) {
-											req.putContext(component.getName() + "." + entry.getKey(), req.getContext(entry.getValue().substring(1)));
-										} else {
-											req.putContext(component.getName() + "." + entry.getKey(), entry.getValue());
-										}
-									}
-								}
-								Widget widget = widgets.getRegistry(req.getVirtualHost()).get(component.getId());
-								if(widget != null && widget.getDataHandler() != null) {
-									return new AbstractMap.SimpleEntry<String,Object>(component.getName(), widget.getDataHandler().handle(req));
+					public void call(Component component) {
+						if(component.getGlobals().size() > 0) {
+							for(Map.Entry<String,String> entry : component.getGlobals().entrySet()) {
+								if(entry.getValue().startsWith(":")) {
+									req.putContext(component.getName() + "." + entry.getKey(), req.getContext(entry.getValue().substring(1)));
 								} else {
-									return new AbstractMap.SimpleEntry<String,Object>(component.getName(), new HashMap<String,Object>());
+									req.putContext(component.getName() + "." + entry.getKey(), entry.getValue());
 								}
 							}
-						});
+						}
+						Widget widget = widgets.getRegistry(req.getVirtualHost()).get(component.getId());
+						if(widget != null && widget.getDataHandler() != null) {
+							widget.getDataHandler().getOptions().setComponentName(component.getName());
+							result.put(component.getName(), widget.getDataHandler().handle(req));
+						} else {
+							result.put(component.getName(), new HashMap<String,Object>());
+						}
 					}
-				}).toBlocking().forEach(new Action1<Entry<String,Object>>() {
-
-					@Override
-					public void call(Entry<String, Object> t1) {
-						result.put(t1.getKey(), t1.getValue());
-						System.out.println(t1.getKey());
-					}
-					
 				});
 				return result;
 			}

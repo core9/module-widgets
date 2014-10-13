6fd5a5473e13585d451036481d50b7ab2e9ce5b5
Mark Wienk <mark.wienk@core9.io>
2014-10-13 21:28:28 +0200

    Added body as default context

--- src/api/java/io/core9/plugin/widgets/datahandler/ContextualDataHandler.java
+++ src/api/java/io/core9/plugin/widgets/datahandler/ContextualDataHandler.java
@@ -2,14 +2,13 @@ package io.core9.plugin.widgets.datahandler;
 
 import io.core9.plugin.server.request.Request;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public abstract class ContextualDataHandler<T extends DataHandlerFactoryConfig> implements DataHandler<T> {
 	
 	@Override
 	public Map<String,Object> handle(Request req) {
-		return handle(req, new HashMap<String, Object>());
+		return handle(req, req.getBodyAsMap().toBlocking().last());
 	}
 
 	public abstract Map<String,Object> handle(Request req, Map<String,Object> context);
